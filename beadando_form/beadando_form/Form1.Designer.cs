aa49f46e2878c1caec43586c4edeb24175e6fcc5
János Kósik <kosikjani05@gmai.com>
2023-05-10 21:33:31 +0200

    kesz

--- beadando_form/beadando_form/Form1.Designer.cs
+++ beadando_form/beadando_form/Form1.Designer.cs
@@ -64,6 +64,7 @@ namespace beadando_form
             this.hozaad1.TabIndex = 1;
             this.hozaad1.Text = "Hozzáad";
             this.hozaad1.UseVisualStyleBackColor = true;
+            this.hozaad1.Click += new System.EventHandler(this.hozaad1_Click);
             // 
             // listBox1
             // 
@@ -72,6 +73,7 @@ namespace beadando_form
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(147, 446);
             this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // legfol
             // 
@@ -81,6 +83,7 @@ namespace beadando_form
             this.legfol.TabIndex = 3;
             this.legfol.Text = "Felülre";
             this.legfol.UseVisualStyleBackColor = true;
+            this.legfol.Click += new System.EventHandler(this.legfol_Click);
             // 
             // fel
             // 
@@ -90,6 +93,7 @@ namespace beadando_form
             this.fel.TabIndex = 4;
             this.fel.Text = "Fel";
             this.fel.UseVisualStyleBackColor = true;
+            this.fel.Click += new System.EventHandler(this.fel_Click);
             // 
             // le
             // 
@@ -99,6 +103,7 @@ namespace beadando_form
             this.le.TabIndex = 5;
             this.le.Text = "Le";
             this.le.UseVisualStyleBackColor = true;
+            this.le.Click += new System.EventHandler(this.le_Click);
             // 
             // legle
             // 
@@ -108,6 +113,7 @@ namespace beadando_form
             this.legle.TabIndex = 6;
             this.legle.Text = "Alulra";
             this.legle.UseVisualStyleBackColor = true;
+            this.legle.Click += new System.EventHandler(this.legle_Click);
             // 
             // torol
             // 
@@ -117,6 +123,7 @@ namespace beadando_form
             this.torol.TabIndex = 7;
             this.torol.Text = "Töröl";
             this.torol.UseVisualStyleBackColor = true;
+            this.torol.Click += new System.EventHandler(this.torol_Click);
             // 
             // balra
             // 
@@ -126,6 +133,7 @@ namespace beadando_form
             this.balra.TabIndex = 8;
             this.balra.Text = "<";
             this.balra.UseVisualStyleBackColor = true;
+            this.balra.Click += new System.EventHandler(this.balra_Click);
             // 
             // jobbra
             // 
@@ -135,6 +143,7 @@ namespace beadando_form
             this.jobbra.TabIndex = 9;
             this.jobbra.Text = ">";
             this.jobbra.UseVisualStyleBackColor = true;
+            this.jobbra.Click += new System.EventHandler(this.jobbra_Click);
             // 
             // textBox2
             // 
@@ -151,6 +160,7 @@ namespace beadando_form
             this.hozzaad2.TabIndex = 11;
             this.hozzaad2.Text = "Hozzáad";
             this.hozzaad2.UseVisualStyleBackColor = true;
+            this.hozzaad2.Click += new System.EventHandler(this.hozzaad2_Click);
             // 
             // listBox2
             // 
@@ -159,6 +169,7 @@ namespace beadando_form
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(147, 446);
             this.listBox2.TabIndex = 12;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // torol2
             // 
@@ -168,6 +179,7 @@ namespace beadando_form
             this.torol2.TabIndex = 13;
             this.torol2.Text = "Töröl";
             this.torol2.UseVisualStyleBackColor = true;
+            this.torol2.Click += new System.EventHandler(this.torol2_Click);
             // 
             // legle2
             // 
@@ -177,6 +189,7 @@ namespace beadando_form
             this.legle2.TabIndex = 14;
             this.legle2.Text = "Alulra";
             this.legle2.UseVisualStyleBackColor = true;
+            this.legle2.Click += new System.EventHandler(this.legle2_Click);
             // 
             // le2
             // 
@@ -186,6 +199,7 @@ namespace beadando_form
             this.le2.TabIndex = 15;
             this.le2.Text = "Le";
             this.le2.UseVisualStyleBackColor = true;
+            this.le2.Click += new System.EventHandler(this.le2_Click);
             // 
             // fel2
             // 
@@ -195,6 +209,7 @@ namespace beadando_form
             this.fel2.TabIndex = 16;
             this.fel2.Text = "Fel";
             this.fel2.UseVisualStyleBackColor = true;
+            this.fel2.Click += new System.EventHandler(this.fel2_Click);
             // 
             // legfel2
             // 
@@ -204,6 +219,7 @@ namespace beadando_form
             this.legfel2.TabIndex = 17;
             this.legfel2.Text = "Felülre";
             this.legfel2.UseVisualStyleBackColor = true;
+            this.legfel2.Click += new System.EventHandler(this.legfel2_Click);
             // 
             // Form1
             // 
--- beadando_form/beadando_form/Form1.cs
+++ beadando_form/beadando_form/Form1.cs
@@ -15,6 +15,199 @@ namespace beadando_form
         public Form1()
         {
             InitializeComponent();
+            fel.Enabled = false;
+            le.Enabled = false;
+            legfol.Enabled = false;
+            legle.Enabled = false;
+            torol.Enabled = false;
+            jobbra.Enabled = false;
+
+            fel2.Enabled = false;
+            le2.Enabled = false;
+            legfel2.Enabled = false;
+            legle2.Enabled = false;
+            torol2.Enabled = false;
+            balra.Enabled = false;
+        }
+
+        private void hozaad1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = string.Empty;
+        }
+
+        private void hozzaad2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox2.Text);
+            textBox2.Text = string.Empty;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                fel.Enabled = true;
+                le.Enabled = true;
+                legfol.Enabled = true;
+                legle.Enabled = true;
+                torol.Enabled = true;
+                jobbra.Enabled = true;
+            }
+            if (listBox1.SelectedItem == null)
+            {
+                fel.Enabled = false;
+                le.Enabled = false;
+                legfol.Enabled = false;
+                legle.Enabled = false;
+                torol.Enabled = false;
+                jobbra.Enabled = false;
+            }
+            if (listBox1.SelectedIndex < 1)
+            {
+                fel.Enabled = false;
+                legfol.Enabled = false;
+            }
+            if (listBox1.SelectedIndex==listBox1.Items.Count-1)
+            {
+                le.Enabled = false;
+                legle.Enabled = false;
+            }
+        }
+
+        private void fel_Click(object sender, EventArgs e)
+        {
+            mozgat(-1);
+        }
+
+        private void mozgat(int irany)
+        {
+            int ujHely = listBox1.SelectedIndex + irany;
+            object mostani = listBox1.SelectedItem;
+
+            listBox1.Items.Remove(mostani);
+            listBox1.Items.Insert(ujHely, mostani);
+            listBox1.SetSelected(ujHely, true);
+        }
+
+        private void torol_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void torol2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Remove(listBox2.SelectedItem);
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                fel2.Enabled = true;
+                le2.Enabled = true;
+                legfel2.Enabled = true;
+                legle2.Enabled = true;
+                torol2.Enabled = true;
+                balra.Enabled = true;
+            }
+            if (listBox2.SelectedItem == null)
+            {
+                fel2.Enabled = false;
+                le2.Enabled = false;
+                legfel2.Enabled = false;
+                legle2.Enabled = false;
+                torol2.Enabled = false;
+                balra.Enabled = false;
+            }
+            if (listBox2.SelectedIndex < 1)
+            {
+                fel2.Enabled = false;
+                legfel2.Enabled = false;
+            }
+            if (listBox2.SelectedIndex == listBox2.Items.Count - 1)
+            {
+                le2.Enabled = false;
+                legle2.Enabled = false;
+            }
+        }
+
+        private void le_Click(object sender, EventArgs e)
+        {
+            mozgat(1);
+        }
+
+        private void legfol_Click(object sender, EventArgs e)
+        {
+            int ujHely = 0;
+            object mostani = listBox1.SelectedItem;
+
+            listBox1.Items.Remove(mostani);
+            listBox1.Items.Insert(ujHely, mostani);
+            listBox1.SetSelected(ujHely, true);
+        }
+
+        private void legle_Click(object sender, EventArgs e)
+        {
+            int ujHely = listBox1.Items.Count-1;
+            object mostani = listBox1.SelectedItem;
+
+            listBox1.Items.Remove(mostani);
+            listBox1.Items.Insert(ujHely, mostani);
+            listBox1.SetSelected(ujHely, true);
+        }
+
+        private void jobbra_Click(object sender, EventArgs e)
+        {
+            object kivalaszt = listBox1.SelectedItem;
+            listBox2.Items.Add(kivalaszt);
+            listBox1.Items.Remove(kivalaszt);
+        }
+        //masik oldal
+        private void balra_Click(object sender, EventArgs e)
+        {
+            object kivalaszt = listBox2.SelectedItem;
+            listBox1.Items.Add(kivalaszt);
+            listBox2.Items.Remove(kivalaszt);
+        }
+
+        private void legfel2_Click(object sender, EventArgs e)
+        {
+            int ujHely = 0;
+            object mostani = listBox2.SelectedItem;
+
+            listBox2.Items.Remove(mostani);
+            listBox2.Items.Insert(ujHely, mostani);
+            listBox2.SetSelected(ujHely, true);
+        }
+
+        private void fel2_Click(object sender, EventArgs e)
+        {
+            mozgat2(-1);
+        }
+
+        private void mozgat2(int irany)
+        {
+            int ujHely = listBox2.SelectedIndex + irany;
+            object mostani = listBox2.SelectedItem;
+
+            listBox2.Items.Remove(mostani);
+            listBox2.Items.Insert(ujHely, mostani);
+            listBox2.SetSelected(ujHely, true);
+        }
+
+        private void le2_Click(object sender, EventArgs e)
+        {
+            mozgat2(1);
+        }
+
+        private void legle2_Click(object sender, EventArgs e)
+        {
+            int ujHely = listBox2.Items.Count - 1;
+            object mostani = listBox2.SelectedItem;
+
+            listBox2.Items.Remove(mostani);
+            listBox2.Items.Insert(ujHely, mostani);
+            listBox2.SetSelected(ujHely, true);
         }
     }
 }
