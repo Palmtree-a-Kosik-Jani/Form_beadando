3a6531ab1d8d0a6dd8c888a6d36dc9233d919f75
János Kósik <kosikjani05@gmai.com>
2023-05-10 20:40:44 +0200

    alap kinezet

---
+++ beadando_form/beadando_form/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace beadando_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.hozaad1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.legfol = new System.Windows.Forms.Button();
+            this.fel = new System.Windows.Forms.Button();
+            this.le = new System.Windows.Forms.Button();
+            this.legle = new System.Windows.Forms.Button();
+            this.torol = new System.Windows.Forms.Button();
+            this.balra = new System.Windows.Forms.Button();
+            this.jobbra = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.hozzaad2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.torol2 = new System.Windows.Forms.Button();
+            this.legle2 = new System.Windows.Forms.Button();
+            this.le2 = new System.Windows.Forms.Button();
+            this.fel2 = new System.Windows.Forms.Button();
+            this.legfel2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // hozaad1
+            // 
+            this.hozaad1.Location = new System.Drawing.Point(165, 12);
+            this.hozaad1.Name = "hozaad1";
+            this.hozaad1.Size = new System.Drawing.Size(75, 23);
+            this.hozaad1.TabIndex = 1;
+            this.hozaad1.Text = "Hozzáad";
+            this.hozaad1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(147, 446);
+            this.listBox1.TabIndex = 2;
+            // 
+            // legfol
+            // 
+            this.legfol.Location = new System.Drawing.Point(165, 82);
+            this.legfol.Name = "legfol";
+            this.legfol.Size = new System.Drawing.Size(75, 23);
+            this.legfol.TabIndex = 3;
+            this.legfol.Text = "Felülre";
+            this.legfol.UseVisualStyleBackColor = true;
+            // 
+            // fel
+            // 
+            this.fel.Location = new System.Drawing.Point(165, 111);
+            this.fel.Name = "fel";
+            this.fel.Size = new System.Drawing.Size(75, 23);
+            this.fel.TabIndex = 4;
+            this.fel.Text = "Fel";
+            this.fel.UseVisualStyleBackColor = true;
+            // 
+            // le
+            // 
+            this.le.Location = new System.Drawing.Point(165, 140);
+            this.le.Name = "le";
+            this.le.Size = new System.Drawing.Size(75, 23);
+            this.le.TabIndex = 5;
+            this.le.Text = "Le";
+            this.le.UseVisualStyleBackColor = true;
+            // 
+            // legle
+            // 
+            this.legle.Location = new System.Drawing.Point(165, 169);
+            this.legle.Name = "legle";
+            this.legle.Size = new System.Drawing.Size(75, 23);
+            this.legle.TabIndex = 6;
+            this.legle.Text = "Alulra";
+            this.legle.UseVisualStyleBackColor = true;
+            // 
+            // torol
+            // 
+            this.torol.Location = new System.Drawing.Point(165, 478);
+            this.torol.Name = "torol";
+            this.torol.Size = new System.Drawing.Size(75, 23);
+            this.torol.TabIndex = 7;
+            this.torol.Text = "Töröl";
+            this.torol.UseVisualStyleBackColor = true;
+            // 
+            // balra
+            // 
+            this.balra.Location = new System.Drawing.Point(290, 169);
+            this.balra.Name = "balra";
+            this.balra.Size = new System.Drawing.Size(75, 23);
+            this.balra.TabIndex = 8;
+            this.balra.Text = "<";
+            this.balra.UseVisualStyleBackColor = true;
+            // 
+            // jobbra
+            // 
+            this.jobbra.Location = new System.Drawing.Point(290, 82);
+            this.jobbra.Name = "jobbra";
+            this.jobbra.Size = new System.Drawing.Size(75, 23);
+            this.jobbra.TabIndex = 9;
+            this.jobbra.Text = ">";
+            this.jobbra.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(396, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // hozzaad2
+            // 
+            this.hozzaad2.Location = new System.Drawing.Point(549, 15);
+            this.hozzaad2.Name = "hozzaad2";
+            this.hozzaad2.Size = new System.Drawing.Size(75, 23);
+            this.hozzaad2.TabIndex = 11;
+            this.hozzaad2.Text = "Hozzáad";
+            this.hozzaad2.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(396, 55);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(147, 446);
+            this.listBox2.TabIndex = 12;
+            // 
+            // torol2
+            // 
+            this.torol2.Location = new System.Drawing.Point(549, 478);
+            this.torol2.Name = "torol2";
+            this.torol2.Size = new System.Drawing.Size(75, 23);
+            this.torol2.TabIndex = 13;
+            this.torol2.Text = "Töröl";
+            this.torol2.UseVisualStyleBackColor = true;
+            // 
+            // legle2
+            // 
+            this.legle2.Location = new System.Drawing.Point(549, 169);
+            this.legle2.Name = "legle2";
+            this.legle2.Size = new System.Drawing.Size(75, 23);
+            this.legle2.TabIndex = 14;
+            this.legle2.Text = "Alulra";
+            this.legle2.UseVisualStyleBackColor = true;
+            // 
+            // le2
+            // 
+            this.le2.Location = new System.Drawing.Point(549, 140);
+            this.le2.Name = "le2";
+            this.le2.Size = new System.Drawing.Size(75, 23);
+            this.le2.TabIndex = 15;
+            this.le2.Text = "Le";
+            this.le2.UseVisualStyleBackColor = true;
+            // 
+            // fel2
+            // 
+            this.fel2.Location = new System.Drawing.Point(549, 111);
+            this.fel2.Name = "fel2";
+            this.fel2.Size = new System.Drawing.Size(75, 23);
+            this.fel2.TabIndex = 16;
+            this.fel2.Text = "Fel";
+            this.fel2.UseVisualStyleBackColor = true;
+            // 
+            // legfel2
+            // 
+            this.legfel2.Location = new System.Drawing.Point(549, 82);
+            this.legfel2.Name = "legfel2";
+            this.legfel2.Size = new System.Drawing.Size(75, 23);
+            this.legfel2.TabIndex = 17;
+            this.legfel2.Text = "Felülre";
+            this.legfel2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 522);
+            this.Controls.Add(this.legfel2);
+            this.Controls.Add(this.fel2);
+            this.Controls.Add(this.le2);
+            this.Controls.Add(this.legle2);
+            this.Controls.Add(this.torol2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.hozzaad2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.jobbra);
+            this.Controls.Add(this.balra);
+            this.Controls.Add(this.torol);
+            this.Controls.Add(this.legle);
+            this.Controls.Add(this.le);
+            this.Controls.Add(this.fel);
+            this.Controls.Add(this.legfol);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.hozaad1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button hozaad1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button legfol;
+        private System.Windows.Forms.Button fel;
+        private System.Windows.Forms.Button le;
+        private System.Windows.Forms.Button legle;
+        private System.Windows.Forms.Button torol;
+        private System.Windows.Forms.Button balra;
+        private System.Windows.Forms.Button jobbra;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button hozzaad2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button torol2;
+        private System.Windows.Forms.Button legle2;
+        private System.Windows.Forms.Button le2;
+        private System.Windows.Forms.Button fel2;
+        private System.Windows.Forms.Button legfel2;
+    }
+}
+
